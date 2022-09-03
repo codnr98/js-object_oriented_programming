857f56b430d626b235f131c2a52679f16da5bb0e
codnr <wnd75913@gmail.com>
2022-09-03 12:25:37 +0900

    객체를 왜 사용하는지

---
+++ built-in.js
@@ -0,0 +1,5 @@
+console.log("Math.PI", Math.PI);
+console.log("Math.floor(3.9)", Math.floor(3.9));
+console.log("Math.random()", Math.random());
+//Math.은 수학과 관련된 변수들을 구룹핑해서 정리한 것
+//수학뿐만아니라 날짜 문자 관련된 수많은 기능들도 있다 그 많은것들을 정리 안된상태로 우리에게 재공된다면 혼란스러울 것이다 그래서 개발자들은 객체를 통해 정리된 데이터를 재공하게된다.
